390af8f9121a57c28efebb185326da62dd2245eb
Sean <sean@jibsales.com>
2014-03-24 07:06:32 -0400

    Rearranged

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp')
   , less = require('gulp-less');
 
-gulp.task('default', function() {
+gulp.task('styles', function() {
   gulp.src('./less/style.less')
     .pipe(less())
     .pipe(gulp.dest('./css'))
 });
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  // gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch('./less/*', ['styles']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['styles', 'watch']);
